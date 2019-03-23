9b7150f238f8897feb9d3b2427f8c738e0a30dc5
Mauricio Carvalho <maurcarvalho@gmail.com>
2019-03-22 21:09:51 -0300

    initial cfg using spring-boot initializer

---
+++ src/main/java/com/trustacademy/api/TrustacademyApiApplication.java
@@ -0,0 +1,13 @@
+package com.trustacademy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustacademyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustacademyApiApplication.class, args);
+	}
+
+}
