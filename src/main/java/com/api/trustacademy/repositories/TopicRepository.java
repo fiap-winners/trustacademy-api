427a31e235b3f77ad3d0e34b7a8c08f5285c47e1
Mauricio Carvalho <maurcarvalho@gmail.com>
2019-04-10 20:14:36 -0300

    follow the same identation for repository

--- src/main/java/com/api/trustacademy/repositories/TopicRepository.java
+++ src/main/java/com/api/trustacademy/repositories/TopicRepository.java
@@ -3,5 +3,4 @@ package com.api.trustacademy.repositories;
 import org.springframework.data.repository.CrudRepository;
 import com.api.trustacademy.models.Topic;
 
-public interface TopicRepository extends CrudRepository<Topic, Long> {
-}
+public interface TopicRepository extends CrudRepository<Topic, Long> {}
