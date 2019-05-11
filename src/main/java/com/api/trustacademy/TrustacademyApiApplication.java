6a766f835020b922f905dee7596ba203cc1122c4
Leo Cristofani <leo.cristofani@gmail.com>
2019-05-11 16:28:36 -0300

    set utf-8 encoding for app and data sources

--- src/main/java/com/api/trustacademy/TrustacademyApiApplication.java
+++ src/main/java/com/api/trustacademy/TrustacademyApiApplication.java
@@ -2,12 +2,27 @@ package com.api.trustacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class TrustacademyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrustacademyApiApplication.class, args);
+	}
+
+	@Bean
+	public Filter getCharacterEncodingFilter() {
+
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+
+		encodingFilter.setEncoding("UTF-8");
+		encodingFilter.setForceEncoding(true);
+
+		return encodingFilter;
 	}
 
 }
